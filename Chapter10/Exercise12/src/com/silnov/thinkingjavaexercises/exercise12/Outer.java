b4c411c6205def94a39b19713794d39dc54a07cd
Сильнов Александр Александрович <al_silnov@mail.ru>
2020-08-17 16:44:51 +0300

    Ex13 - Ex14 were added to Chapter10

--- Chapter10/Exercise12/src/com/silnov/thinkingjavaexercises/exercise12/Outer.java
+++ Chapter10/Exercise12/src/com/silnov/thinkingjavaexercises/exercise12/Outer.java
@@ -1,9 +1,20 @@
 package com.silnov.thinkingjavaexercises.exercise12;
 
+class Inner {
+    Inner() {
+        System.out.println("Inner()");
+    }
+    void methodInner() {
+    }
+}
 public class Outer {
     private String feild;
     Outer() {
         System.out.println("Outer()");
+    }
+    private void methodOuter() {
+        System.out.println("Start methodOuter()");
+        System.out.println("Finish methodOuter()");
     }
     void methodOuterTwo() {
         System.out.println("Start methodOuterTwo()");
@@ -19,6 +30,7 @@ public class Outer {
         in.methodInner();
         System.out.println("Finish methodOuterTwo()");
     }
+
     public static void main(String[] args) {
         Outer out = new Outer();
         out.methodOuterTwo();
