7b70b4745587cfea7c5dd1d6a9e67ac10d67bbb4
Сильнов Александр Александрович <al_silnov@mail.ru>
2020-08-19 16:59:34 +0300

    Ex20 - Ex23 were added to Chapter10

---
+++ Chapter10/Exercise22/src/com/silnov/thinkingjavaexercises/exercise22/Sequence.java
@@ -0,0 +1,90 @@
+package com.silnov.thinkingjavaexercises.exercise22;
+
+class Word {
+    String word;
+    Word(String word) {
+        this.word = word;
+    }
+    public String toString() {
+        return word;
+    }
+}
+interface Selector {
+    boolean end();
+    Object current();
+    void next();
+}
+
+public  class Sequence {
+    private Object[] items;
+    private int next = 0;
+    public Sequence(int size) {
+        items = new Object[size];
+    }
+	public void add(Object x) {
+		if(next < items.length) {
+            items[next++] = x;
+        }
+    }
+    @Override
+    public String toString() {
+        return "Sequence";
+    }
+	private class SequenceSelector implements Selector {
+		private int i = 0;
+		public boolean end() {
+            return i == items.length;
+        }
+		public Object current() {
+            return items[i];
+        }
+		public void next() {
+            if(i < items.length) {
+                ++i;
+            }
+        }
+        public Sequence outer() {
+            return Sequence.this;
+        } 
+    }
+    private class ReverseSequenceSelector implements Selector {
+		private int i = items.length - 1;
+		public boolean end() {
+            return i == -1;
+        }
+		public Object current() {
+            return items[i];
+        }
+		public void next() {
+            if(i > -1) {
+                --i;
+            }
+        }
+        public Sequence outer() {
+            return Sequence.this;
+        } 
+	}
+	public Selector selector() {
+		return new SequenceSelector();
+    }
+    public Selector reverseSelector() {
+		return new ReverseSequenceSelector();
+	}
+    public static void main(String[] args) {
+        Sequence seq = new Sequence(10);
+        Selector sel  = seq.selector();
+        for(int i = 0; i < 10; ++i) {
+            seq.add(new Word(Integer.toString(i)));
+        }
+        System.out.println(((SequenceSelector)sel).outer());
+        while(!sel.end()) {
+            System.out.println(sel.current());
+            sel.next();
+        }
+        sel  = seq.reverseSelector();
+        while(!sel.end()) {
+            System.out.println(sel.current());
+            sel.next();
+        }
+    }
+}
