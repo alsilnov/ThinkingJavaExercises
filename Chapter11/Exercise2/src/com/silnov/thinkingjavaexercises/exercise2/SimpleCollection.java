d603532815ff08e8d625f69394ff85fdca15d96c
Сильнов Александр Александрович <al_silnov@mail.ru>
2020-08-24 17:19:11 +0300

    Ex2 was added to Chapter11

---
+++ Chapter11/Exercise2/src/com/silnov/thinkingjavaexercises/exercise2/SimpleCollection.java
@@ -0,0 +1,13 @@
+package com.silnov.thinkingjavaexercises.exercise2;
+
+import java.util.*;
+
+public class SimpleCollection {
+  public static void main(String[] args) {
+    Set<Integer> c = new HashSet<Integer>();
+    for(int i = 0; i < 10; i++)
+      c.add(i); // Autoboxing
+    for(Integer i : c)
+      System.out.print(i + ", ");
+  }
+}
