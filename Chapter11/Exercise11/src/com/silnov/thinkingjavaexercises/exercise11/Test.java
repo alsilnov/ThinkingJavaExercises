019b772b4b6aca37440c07f405ab624dc23e9a15
Сильнов Александр Александрович <al_silnov@mail.ru>
2020-08-31 10:19:56 +0300

    Ex11 were added to Chapter11

---
+++ Chapter11/Exercise11/src/com/silnov/thinkingjavaexercises/exercise11/Test.java
@@ -0,0 +1,31 @@
+package com.silnov.thinkingjavaexercises.exercise11;
+
+import java.util.*;
+import java.text.SimpleDateFormat;
+
+public class Test {
+    static void method(Collection col) {
+        Iterator it = col.iterator();
+        while (it.hasNext()) {
+            System.out.print(it.next() + " ");
+        }
+        System.out.println("");
+    }
+    public static void main(String[] args) {
+    ArrayList<Integer> al = 
+        new ArrayList<Integer>(Arrays.asList(1, 2, 3));
+    LinkedList<Character> ll =
+        new LinkedList<Character>(Arrays.asList('a', 'b', 'c'));	
+    HashSet<Float> hs = 
+        new HashSet<Float>(Arrays.asList(1.1f, 2.2f, 3.3f));
+    TreeSet<Double> ts =
+        new TreeSet<Double>(Arrays.asList(1.11, 2.22, 3.33));
+    LinkedHashSet<Integer> lhs =
+        new LinkedHashSet<Integer>(Arrays.asList(11, 22, 33));
+    method(al);
+    method(ll);
+    method(hs);
+    method(ts);
+    method(lhs);
+    }
+}
