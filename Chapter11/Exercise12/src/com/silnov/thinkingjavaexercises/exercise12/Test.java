f8e02cdf217d3e0ddbc08711198d2f1d4ffaecab
Сильнов Александр Александрович <al_silnov@mail.ru>
2020-09-02 11:12:12 +0300

    Ex12 was modified in Chapter11

--- Chapter11/Exercise12/src/com/silnov/thinkingjavaexercises/exercise12/Test.java
+++ Chapter11/Exercise12/src/com/silnov/thinkingjavaexercises/exercise12/Test.java
@@ -6,8 +6,7 @@ import java.util.*;
  * Test
  */
 public class Test {
-
-  public static void main(String[] args) {
+    public static void main(String[] args) {
     List<Integer> list1 = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8));
     List<Integer> list2 = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8));
     ListIterator<Integer> it1 = list1.listIterator();
