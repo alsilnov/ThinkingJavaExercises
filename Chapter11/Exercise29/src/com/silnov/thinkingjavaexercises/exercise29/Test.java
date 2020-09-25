40fae46cdbcce49de4482da1d3dd23b8c1e50431
Сильнов Александр Александрович <al_silnov@mail.ru>
2020-09-25 13:28:54 +0300

    Ex29 was added to Chapter11

---
+++ Chapter11/Exercise29/src/com/silnov/thinkingjavaexercises/exercise29/Test.java
@@ -0,0 +1,18 @@
+package com.silnov.thinkingjavaexercises.exercise29;
+
+import java.util.*;
+/**
+ * Simple
+ */
+class Simple extends Object {
+}
+/**
+ * Test
+ */
+public class Test {
+  public static void main(String[] args) {
+    PriorityQueue<Simple> pq = new PriorityQueue<Simple>();
+    //pq.offer(new Simple());
+    //pq.offer(new Simple());
+  }
+}
