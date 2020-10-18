e2b7ec98196da4fdd9f2b16974c0ab7c744d75b0
Alexandr Silnov <al_silnov@mail.ru>
2020-10-18 17:58:20 +0300

    Ex32 was added to Chapter11

---
+++ Chapter11/Exercise32/src/com/silnov/thinkingjavaexercises/exercise32/pets/Rodent.java
@@ -0,0 +1,10 @@
+// typeinfo/pets/Rodent.java
+// (c)2020 MindView LLC: see Copyright.txt
+// We make no guarantees that this code is fit for any purpose.
+// Visit http://OnJava8.com for more book information.
+package com.silnov.thinkingjavaexercises.exercise32.pets;
+
+public class Rodent extends Pet {
+  public Rodent(String name) { super(name); }
+  public Rodent() { super(); }
+}
