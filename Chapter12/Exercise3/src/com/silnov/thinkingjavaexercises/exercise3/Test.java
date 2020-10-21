4441879c0a6e45814373af4ddbdfa12a80bf7537
Сильнов Александр Александрович <al_silnov@mail.ru>
2020-10-21 11:22:02 +0300

    Ex3 was added to Chapter12

---
+++ Chapter12/Exercise3/src/com/silnov/thinkingjavaexercises/exercise3/Test.java
@@ -0,0 +1,12 @@
+package com.silnov.thinkingjavaexercises.exercise3;
+
+public class Test {  
+  public static void main(String[] args) {
+    String[] str = {"0"};
+    try {
+      System.out.println(str[1]);
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+  }
+}
