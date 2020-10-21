280174515ae01202c1bd21189e5a948152d842c6
Сильнов Александр Александрович <al_silnov@mail.ru>
2020-10-21 11:33:41 +0300

    Ex5 was added to Chapter12

---
+++ Chapter12/Exercise5/src/com/silnov/thinkingjavaexercises/exercise5/Test.java
@@ -0,0 +1,19 @@
+package com.silnov.thinkingjavaexercises.exercise5;
+
+public class Test extends Exception {
+
+  public static void main(String[] args) {
+    int[] iArray = {1};
+    int i = 5;
+    while(true){
+      try {
+        iArray[i] = 5;
+        System.out.println("OK");
+        break;
+      } catch (IndexOutOfBoundsException e) {
+        e.printStackTrace();
+        i--;
+      }
+    }
+  }
+}
