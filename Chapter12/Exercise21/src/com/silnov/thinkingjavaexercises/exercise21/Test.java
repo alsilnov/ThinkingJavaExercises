e3854ed8b53249a20fcba8abaf020a8f8d651b4d
Сильнов Александр Александрович <al_silnov@mail.ru>
2020-11-10 16:02:31 +0300

    Ex21 was added to Chapter12

---
+++ Chapter12/Exercise21/src/com/silnov/thinkingjavaexercises/exercise21/Test.java
@@ -0,0 +1,28 @@
+package com.silnov.thinkingjavaexercises.exercise21;
+
+/**
+ * AException extends Exception
+ */
+class AException extends Exception {}
+/**
+ * A
+ */
+class A {
+  A() throws AException {
+    throw new AException();
+  }
+}
+public class Test extends A {
+  Test() throws AException{
+    // try {
+		//	super();
+		// } catch(BaseException e) {}
+  }
+  public static void main(String[] args) {
+    try {
+      Test test = new Test();
+    } catch (AException e) {
+      System.out.println("AException caught in main()");
+    }
+  }
+}
