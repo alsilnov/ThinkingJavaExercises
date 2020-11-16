fdb5ab22c1492873e259c5b8d0d79795ad245d25
Сильнов Александр Александрович <al_silnov@mail.ru>
2020-11-16 10:33:02 +0300

    Ex25 was added to Chapter12

---
+++ Chapter12/Exercise25/src/com/silnov/thinkingjavaexercises/exercise25/Test.java
@@ -0,0 +1,33 @@
+package com.silnov.thinkingjavaexercises.exercise25;
+class AException extends Exception {}
+class BException extends AException {}
+class CException extends BException {}
+class A {
+	void f() throws AException {
+		throw new AException();
+	}
+}
+class B extends A {
+	void f() throws BException {
+		throw new BException();
+	}
+}
+class C extends B {
+	void f() throws CException {
+		throw new CException();
+	}
+}
+public class Test {
+	public static void main(String[] args) {		
+		A a = new C();
+		try {
+			a.f();
+		}	catch(CException e3) {
+			System.out.println("Caught e3");			
+		}	catch(BException e2) {
+			System.out.println("Caught e2");
+		}	catch(AException e1) {
+			System.out.println("Caught e1");
+		}		
+	}
+}
