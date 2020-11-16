a0f33081bdc84955b1c4cf273d7606dbafdaf6ec
Сильнов Александр Александрович <al_silnov@mail.ru>
2020-11-16 16:56:55 +0300

    Ex26 was added to Chapter12

---
+++ Chapter12/Exercise26/src/com/silnov/thinkingjavaexercises/exercise26/MainException.java
@@ -0,0 +1,15 @@
+package com.silnov.thinkingjavaexercises.exercise26;
+import java.util.*;
+import java.nio.file.*;
+
+public class MainException {
+  // Pass exceptions to the console:
+  private static String fileInExercisePath = "Exercise26/src/com/silnov/thinkingjavaexercises/exercise26/";
+	private static String exercisePath = System.getProperty("user.dir") + "/";
+  public static void main(String[] args) throws Exception {
+    // Open the file:
+    List<String> lines = Files.readAllLines(
+      Paths.get(exercisePath + fileInExercisePath + "MainException.java" + "removreIt"));
+    // Use the file ...
+  }
+}
