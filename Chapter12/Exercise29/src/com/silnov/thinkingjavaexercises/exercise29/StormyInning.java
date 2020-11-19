eca4729cb52c898a37b83c0c43d96ee960f9f74b
Сильнов Александр Александрович <al_silnov@mail.ru>
2020-11-19 11:17:54 +0300

    Ex29 was added to Chapter12

---
+++ Chapter12/Exercise29/src/com/silnov/thinkingjavaexercises/exercise29/StormyInning.java
@@ -0,0 +1,36 @@
+package com.silnov.thinkingjavaexercises.exercise29;
+
+class BaseballException extends Exception {}
+class Foul extends RuntimeException {}
+class Strike extends RuntimeException {}
+abstract class Inning {
+  Inning() {}
+  public void event() {}
+  public abstract void atBat();
+  public void walk() {}
+}
+class StormException extends RuntimeException {}
+class RainedOut extends RuntimeException {}
+class PopFoul extends RuntimeException {}
+interface Storm {
+  void event();
+  void rainHard();
+}
+public class StormyInning extends Inning implements Storm {
+  public StormyInning() {}
+  public StormyInning(String s) {}
+  @Override
+  public void rainHard() {}
+  @Override
+  public void event() {}
+  @Override
+  public void atBat() {
+    throw new Strike();
+  }
+  public static void main(String[] args) {
+    StormyInning si = new StormyInning();
+    si.atBat();
+    Inning i = new StormyInning();
+    i.atBat();
+  }
+}
