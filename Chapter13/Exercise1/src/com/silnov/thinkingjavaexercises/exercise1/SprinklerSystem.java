f57c19ec590aa59afce32c7bdca10ae48d09fb84
Сильнов Александр Александрович <al_silnov@mail.ru>
2021-02-09 11:37:25 +0300

    Chapter13 was created. Ex1 was added to Chapter13

---
+++ Chapter13/Exercise1/src/com/silnov/thinkingjavaexercises/exercise1/SprinklerSystem.java
@@ -0,0 +1,49 @@
+package com.silnov.thinkingjavaexercises.exercise1;
+import java.util.*;
+
+import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
+class WaterSource {
+  private String s;
+  WaterSource() {
+    System.out.println("WaterSource()");
+    s = "Constructed";
+  }
+  @Override public String toString() { return s; }
+}
+
+public class SprinklerSystem {
+  private String valve1, valve2, valve3, valve4;
+  private WaterSource source = new WaterSource();
+  private int i;
+  private float f;
+  @Override public String toString() {
+    StringBuilder result = new StringBuilder();
+    result.append("valve1 = ");
+    result.append(valve1);
+    result.append(" valve2 = ");
+    result.append(valve2);
+    result.append(" valve3 = ");
+    result.append(valve3);
+    result.append(" valve4 = ");
+    result.append(valve4);
+    result.append("\ni = ");
+    result.append(i);
+    result.append(" f = ");
+    result.append(f);
+    result.append(" source = ");
+    result.append(source);
+    return result.toString();
+    
+    /*return
+      "valve1 = " + valve1 + " " +
+      "valve2 = " + valve2 + " " +
+      "valve3 = " + valve3 + " " +
+      "valve4 = " + valve4 + "\n" +
+      "i = " + i + " " + "f = " + f + " " +
+      "source = " + source;                      // [1]*/
+  }
+  public static void main(String[] args) {
+    SprinklerSystem sprinklers = new SprinklerSystem();
+    System.out.println(sprinklers);
+  }
+}
