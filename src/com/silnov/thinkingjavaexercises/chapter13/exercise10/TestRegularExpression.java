012b9331ed1cd1a542f5a681b49c21de1915503b
Сильнов Александр Александрович <al_silnov@mail.ru>
2021-02-24 17:13:21 +0300

    Ex10 has been added to Chapter13

---
+++ src/com/silnov/thinkingjavaexercises/chapter13/exercise10/TestRegularExpression.java
@@ -0,0 +1,28 @@
+package com.silnov.thinkingjavaexercises.chapter13.exercise10;
+
+import java.io.PrintStream;
+import java.util.regex.*;
+
+public class TestRegularExpression {
+	public static void main(String[] args) {
+		if(args.length < 2) {
+			System.out.println("Usage:\njava TestRegularExpression " + 
+				"characterSequence regularExpression+");
+			System.exit(0);  
+		}
+		System.out.println("Input: \"" + args[0] + "\"");
+		for(String arg : args) {
+			System.out.println("Regular expression: \"" + arg + "\"");
+			Pattern p = Pattern.compile(arg);
+			Matcher m = p.matcher(args[0]);
+			if(!m.find()) 
+			  	System.out.println("No match found for " + "\"" + arg + "\"");
+			m.reset();
+			while(m.find()) {
+				System.out.println("Match \"" + m.group() + "\" at position " +
+					m.start() + ((m.end() - m.start() < 2) ? "" :  ("-" + (m.end() - 1))));
+			}
+		}
+
+	}
+}
