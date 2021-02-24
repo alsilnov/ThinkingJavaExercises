474521e1489b425b9bbccc47d9abe19f9c5e3af2
Сильнов Александр Александрович <al_silnov@mail.ru>
2021-02-24 10:38:07 +0300

    Ex7 has been added to Chapter13

---
+++ src/com/silnov/thinkingjavaexercises/chapter13/exercise7/Test.java
@@ -0,0 +1,15 @@
+package com.silnov.thinkingjavaexercises.chapter13.exercise7;
+
+import java.util.*;
+/**
+ * Test
+ */
+public class Test {
+	public static String str1 = "Asdasdasdasdqwdqdd.";
+	public static String str2 = "asdasdasdasdqwdqdd."; 
+	public static void main(String[] args) {
+		System.out.println(str1.matches("^[A-Z].*[\\.]$"));
+		System.out.println(str2.matches("^[A-Z].*[\\.]$"));
+	}
+	
+}
