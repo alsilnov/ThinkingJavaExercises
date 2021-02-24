9597b33479f057bc9458ff63296296d41e834ba0
Сильнов Александр Александрович <al_silnov@mail.ru>
2021-02-24 10:45:33 +0300

    Ex9 has been added to Chapter13

---
+++ src/com/silnov/thinkingjavaexercises/chapter13/exercise9/Test.java
@@ -0,0 +1,16 @@
+package com.silnov.thinkingjavaexercises.chapter13.exercise9;
+
+import java.util.*;
+/**
+ * Test
+ */
+public class Test {
+	public static String knights =
+    "Then, when you have found the shrubbery, " +
+    "you must cut down the mightiest tree in the " +
+    "forest...with... a herring!";
+	public static void main(String[] args) {
+		System.out.println(knights.replaceAll("[aeiouAEIOU]", "_"));
+	}
+	
+}
