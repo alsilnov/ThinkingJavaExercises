bf10c1229960efb2282136eac0765130ffa12e6c
Сильнов Александр Александрович <al_silnov@mail.ru>
2021-03-17 15:14:20 +0300

    Ex16 has been added to Chapter14

---
+++ src/com/silnov/thinkingjavaexercises/chapter14/exercise16/CofeeFactoryTest.java
@@ -0,0 +1,61 @@
+package com.silnov.thinkingjavaexercises.chapter14.exercise16;
+import java.util.*;
+
+class Coffee {
+	private static long counter = 0;
+	private final long id = counter++;
+	public String toString() {
+		return getClass().getSimpleName() + " " + id;
+	}
+	static List<Factory<? extends Coffee>> coffeeFactories =
+		new ArrayList<Factory<? extends Coffee>>();
+	static {
+		coffeeFactories.add(new Latte.Factory());
+		coffeeFactories.add(new Mocha.Factory());
+		coffeeFactories.add(new Cappuccino.Factory());
+		coffeeFactories.add(new Americano.Factory());
+		coffeeFactories.add(new Breve.Factory());
+	}
+	private static Random rand = new Random();
+	public static Coffee createRandom() {
+		int n = rand.nextInt(coffeeFactories.size());
+		return coffeeFactories.get(n).create();
+	}
+}
+
+class Latte extends Coffee {
+	public static class Factory implements com.silnov.thinkingjavaexercises.chapter14.exercise16.Factory<Latte> {
+		public Latte create() { return new Latte(); }
+	}
+}
+
+class Mocha extends Coffee {
+	public static class Factory implements com.silnov.thinkingjavaexercises.chapter14.exercise16.Factory<Mocha> {
+		public Mocha create() { return new Mocha(); }
+	}
+}
+
+class Cappuccino extends Coffee {
+	public static class Factory implements com.silnov.thinkingjavaexercises.chapter14.exercise16.Factory<Cappuccino> {
+		public Cappuccino create() { return new Cappuccino(); }
+	}
+}
+
+class Americano extends Coffee {
+	public static class Factory implements com.silnov.thinkingjavaexercises.chapter14.exercise16.Factory<Americano> {
+		public Americano create() { return new Americano(); }
+	}
+}
+
+class Breve extends Coffee {
+	public static class Factory implements com.silnov.thinkingjavaexercises.chapter14.exercise16.Factory<Breve> {
+		public Breve create() { return new Breve(); }
+	}
+}
+
+public class CofeeFactoryTest {
+	public static void main(String[] args) {
+		for(int i = 0; i < 15; i++)
+			System.out.println(Coffee.createRandom());
+	}
+}
---
+++ src/com/silnov/thinkingjavaexercises/chapter14/exercise16/Factory.java
@@ -0,0 +1,5 @@
+package com.silnov.thinkingjavaexercises.chapter14.exercise16;
+
+public interface Factory<T> {
+    T create();
+}
