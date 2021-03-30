746e8e68730c22c834fa2130cdb5c703449c8c59
Сильнов Александр Александрович <al_silnov@mail.ru>
2021-03-30 09:26:10 +0300

    Ex3 has been modified in Chapter15

---
+++ src/com/silnov/thinkingjavaexercises/chapter15/exercise3/Holder.java
@@ -0,0 +1,8 @@
+package com.silnov.thinkingjavaexercises.chapter15.exercise3;
+
+public class Holder {
+    public static void main(String[] args) {
+        Tuple6<Integer, Integer, Integer,Integer,Integer,Integer> tup = new Tuple6<>(1, 2, 3, 4, 5, 6);
+        System.out.println(tup.a1); 
+    }
+}
---
+++ src/com/silnov/thinkingjavaexercises/chapter15/exercise3/Tuple6.java
@@ -0,0 +1,16 @@
+// onjava/Tuple5.java
+// (c)2021 MindView LLC: see Copyright.txt
+// We make no guarantees that this code is fit for any purpose.
+// Visit http://OnJava8.com for more book information.
+package com.silnov.thinkingjavaexercises.chapter15.exercise3;
+public class Tuple6<A, B, C, D, E, F>
+extends Tuple5<A, B, C, D, E> {
+  public final F a6;
+  public Tuple6(A a, B b, C c, D d, E e, F f) {
+    super(a, b, c, d, e);
+    a6 = f;
+  }
+  @Override public String rep() {
+    return super.rep() + ", " + a6;
+  }
+}
