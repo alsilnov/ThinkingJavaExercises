4cf8fe39b1e47a837e3d1251dbaaba7d32a12103
Сильнов Александр Александрович <al_silnov@mail.ru>
2021-04-14 11:58:37 +0300

    Ex18 has been modified in Chapter15

--- src/com/silnov/thinkingjavaexercises/chapter15/exercise18/RandomList.java
+++ src/com/silnov/thinkingjavaexercises/chapter15/exercise18/RandomList.java
@@ -2,7 +2,7 @@
 // (c)2021 MindView LLC: see Copyright.txt
 // We make no guarantees that this code is fit for any purpose.
 // Visit http://OnJava8.com for more book information.
-package com.silnov.thinkingjavaexercises.chapter15.exercise6;
+package com.silnov.thinkingjavaexercises.chapter15.exercise18;
 import java.util.*;
 import java.util.stream.*;
 
