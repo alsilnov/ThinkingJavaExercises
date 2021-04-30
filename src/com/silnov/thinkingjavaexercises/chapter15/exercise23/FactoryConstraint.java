c5bd6f3515db4947642ba6e9fd8ed9261e8730cf
Alex Silnov <al_silnov@mail.ru>
2021-04-30 13:06:30 +0300

    Ex23 has been added to chapter15

---
+++ src/com/silnov/thinkingjavaexercises/chapter15/exercise23/FactoryConstraint.java
@@ -0,0 +1,48 @@
+package com.silnov.thinkingjavaexercises.chapter15.exercise23;
+
+interface FactoryI<T> {
+    T create(Integer i);
+}
+
+class Foo2<T> {
+    private T x;
+
+    public void get() {
+        System.out.println(x);
+    }
+
+    public <F extends FactoryI<T>> Foo2(F factory, Integer i) {
+        x = factory.create(i);
+    }
+}
+
+class IntegerFactory implements FactoryI<Integer> {
+    public Integer create(Integer i) {
+        return new Integer(i);
+    }
+}
+
+class Widget {
+    int i;
+
+    public static class Factory implements FactoryI<Widget> {
+        public Widget create(Integer i) {
+            Widget w = new Widget();
+            w.i = i;
+            return w;
+        }
+    }
+
+    public String toString() {
+        return "Widget " + i;
+    }
+}
+
+public class FactoryConstraint {
+    public static void main(String[] args) {
+        Foo2<Integer> f1 = new Foo2<>(new IntegerFactory(), 1);
+        Foo2<Widget> f2 = new Foo2<>(new Widget.Factory(), 2);
+        f1.get();
+        f2.get();
+    }
+}
