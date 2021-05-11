d109cac4ad236c99a4d4164184d92d59ac045eba
Alex Silnov <al_silnov@mail.ru>
2021-05-11 15:17:19 +0300

    Ex2 has been added to Chapter16

---
+++ src/com/silnov/thinkingjavaexercises/chapter16/exercise2/Test.java
@@ -0,0 +1,27 @@
+package com.silnov.thinkingjavaexercises.chapter16.exercise2;
+
+import java.text.BreakIterator;
+import java.util.Arrays;
+
+class BerylliumSphere {
+    private static long counter;
+    private final long id = counter++;
+
+    public String toString() {
+        return "Sphere " + id;
+    }
+}
+
+public class Test {
+    static BerylliumSphere[] foo(int n) {
+        BerylliumSphere[] a = new BerylliumSphere[n];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = new BerylliumSphere();
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(foo(5)));
+    }
+}
