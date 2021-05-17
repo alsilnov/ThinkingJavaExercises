44981ca763e94acaabd8aec3ee6ee2dcabe62e57
Alex Silnov <al_silnov@mail.ru>
2021-05-17 09:49:37 +0300

    Ex9 has been added to Chapter16

---
+++ src/com/silnov/thinkingjavaexercises/chapter16/exercise9/Test.java
@@ -0,0 +1,38 @@
+package com.silnov.thinkingjavaexercises.chapter16.exercise9;
+
+import java.util.*;
+
+class Banana {
+    private static long counter;
+    private final long id = counter++;
+
+    public String toString() {
+        return "Banana " + id;
+    }
+}
+
+class Peel<T> {
+    private T t;
+    private static long counter;
+    private final long id = counter++;
+
+    public Peel(T t) {
+        this.t = t;
+    }
+
+    public String toString() {
+        return "Peel " + id + " " + t.toString() + " class: " + t.getClass().getSimpleName();
+    }
+}
+
+public class Test {
+    public static void main(String[] args) {
+        Peel<Banana> peel = new Peel<>(new Banana());
+        System.out.println(peel);
+        List<Peel<Banana>> peels = new ArrayList<Peel<Banana>>();
+        for (int i = 0; i < 10; i++)
+            peels.add(new Peel<Banana>(new Banana()));
+        System.out.println(peels);
+        System.out.println("peels.get(0).getClass(): " + peels.get(0).getClass().getSimpleName());
+    }
+}
