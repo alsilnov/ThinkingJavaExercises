ff47282f54026dd06e3aefeebd5a6eeb0253fba8
Alex Silnov <al_silnov@mail.ru>
2021-05-17 09:50:10 +0300

    Ex11 has been added to Chapter16

---
+++ src/com/silnov/thinkingjavaexercises/chapter16/exercise11/Test.java
@@ -0,0 +1,10 @@
+package com.silnov.thinkingjavaexercises.chapter16.exercise11;
+
+public class Test {
+    public static void main(String[] args) {
+        int[] i = new int[10];
+        Integer[] iI = new Integer[10];
+        // Type mismatch: cannot convert from int[] to Integer[]
+        // iI = i;
+    }
+}
