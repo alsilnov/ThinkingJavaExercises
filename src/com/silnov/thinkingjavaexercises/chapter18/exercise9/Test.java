d5f276d99ad2ef7dffb3af529b3808afef4a9be8
Alex Silnov <al_silnov@mail.ru>
2021-07-29 13:37:13 +0300

    Ex9 has been added to Chapter18

---
+++ src/com/silnov/thinkingjavaexercises/chapter18/exercise9/Test.java
@@ -0,0 +1,42 @@
+package src.com.silnov.thinkingjavaexercises.chapter18.exercise9;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("------------------------------------------------------------------");
+        String fileName;
+        if (args.length == 1) {
+            fileName = args[0];
+        } else {
+            fileName = "src\\com\\silnov\\thinkingjavaexercises\\chapter18\\exercise8\\Test.java";
+        }
+        File f = new File(fileName);
+        System.out.println(f.getAbsolutePath());
+        System.out.println("------------------------------------------------------------------");
+        LinkedList<String> list = new LinkedList<>();
+        try (BufferedReader in = new BufferedReader(new FileReader(f));) {
+            String s;
+            while ((s = in.readLine()) != null) {
+                list.add(s.toUpperCase());
+            }
+            for (String string : list) {
+                System.out.println(string);
+            }
+            System.out.println("------------------------------------------------------------------");
+            Collections.reverse(list);
+            for (String string : list) {
+                System.out.println(string);
+            }
+            System.out.println("------------------------------------------------------------------");
+        } catch (Exception e) {
+            System.out.println("IOException");
+        }
+    }
+}
